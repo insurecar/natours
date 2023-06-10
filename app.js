d2e9e3ac97d6a41b6d6b1ee1313ae97571a993ab
insurecar <rostyslavshyian@gmail.com>
2023-06-10 17:17:11 +0200

    finished 53

--- app.js
+++ app.js
@@ -1,19 +1,55 @@
 const express = require('express');
+const fs = require('fs');
 
 const app = express();
 
+app.use(express.json());
+
+// app.get('/', (req, res) => {
+//   res
+//   .status(200)
+//   .json({ message: 'Hello from the server side!', app: 'Natours' });
+// });
+
+// app.post('/', (req, res) => {
+//   res.json({ message: 'You can post to this endpoint' });
+// });
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: ' success',
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  // console.log(req.body);
+
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'succcess',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
+});
+
 const port = 3000;
-
-app.get('/', (req, res) => {
-  res
-    .status(200)
-    .json({ message: 'Hello from the server side!', app: 'Natours' });
-});
-
-app.post('/', (req, res) => {
-  res.json({ message: 'You can post to this endpoint' });
-});
-
 app.listen(port, () => {
   console.log(`App is running on port ${port}...`);
 });

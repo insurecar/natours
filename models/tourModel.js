511b5ee2bc858d7abe00c8db8a5782583255ef5b
insurecar <rostyslavshyian@gmail.com>
2023-10-27 13:06:57 +0200

    added middleware 106

--- models/tourModel.js
+++ models/tourModel.js
@@ -55,6 +55,10 @@ const tourSchema = new mongoose.Schema(
       select: false, //hide this field
     },
     startDates: [Date],
+    secretTour: {
+      type: Boolean,
+      default: false,
+    },
   },
   {
     toJSON: {
@@ -86,6 +90,20 @@ tourSchema.pre("save", function (next) {
 
 //   next();
 // });
+
+//QUERY MIDDLWARE
+tourSchema.pre(/^find/, function (next) {
+  this.find({ secretTour: { $ne: true } }); // if this trigger is true we will not get this tour
+
+  this.start = Date.now();
+  next();
+});
+
+tourSchema.post(/^find/, function (docs, next) {
+  console.log(`Query took ${Date.now() - this.start} milliseconds 😃😃😃😃😃`);
+  console.log(docs);
+  next();
+});
 
 const Tour = mongoose.model("Tour", tourSchema);
 

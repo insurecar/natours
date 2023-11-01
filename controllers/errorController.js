0ffe75bf1be4286f4d97b57d84960ecd982f5188
insurecar <rostyslavshyian@gmail.com>
2023-11-01 17:06:34 +0100

    handle duplicate 120

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -2,7 +2,7 @@ const AppError = require("../utils/appError");
 
 const handleCastErrorDB = (err) => {
   const message = `Invalid ID ${err.path}: ${err.value}.`;
-  return AppError(message, 404);
+  return new AppError(message, 400);
 };
 
 const sendErrorDev = (err, res) => {
@@ -12,6 +12,12 @@ const sendErrorDev = (err, res) => {
     message: err.message,
     stack: err.stack,
   });
+};
+
+const handleDuplicateFieldsDB = (err) => {
+  const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
+  const message = `Duplicate field value: ${value}. Please use another value!`;
+  return new AppError(message, 400);
 };
 
 const sendErrorProduction = (err, res) => {
@@ -43,6 +49,7 @@ module.exports = (err, req, res, next) => {
   } else if (process.env.NODE_ENV === "production") {
     let error = { ...err };
     if (error.name === "CastError") error = handleCastErrorDB(error);
+    if (error.code === 11000) error = handleDuplicateFieldsDB(error);
 
     sendErrorProduction(error, res);
   }

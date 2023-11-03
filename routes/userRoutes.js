463b965a48ba352100cba3df8890f577e3ee61ec
insurecar <rostyslavshyian@gmail.com>
2023-11-03 12:39:39 +0100

    created first user 126

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -7,9 +7,13 @@ const {
   deleteUser,
 } = require("../controllers/userController");
 
+const { signup } = require("../controllers/authController");
+
 const router = express.Router();
 
-router.route("/").get(getAllUsers);
+router.post("/signup", signup);
+
+router.route("/").get(getAllUsers).post(createUser);
 
 router.route("/:id").get(getUser).patch(updateUser).delete(deleteUser);
 
